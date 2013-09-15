856d7cf44872e53c604135b50d49b09cf0a377f2
Aliou Diallo <aliou.diallo@me.com>
2013-09-15 14:10:41 +0200

    Create Mock class and edit Mutex test.

---
+++ test/mockclass.hh
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Thread/Mutex.hh"
+
+#ifndef MOCKCLASS_HH_
+# define MOCKCLASS_HH_
+
+class Mock
+{
+  private:
+    int _data;
+    LilWrapper::IMutex *_m;
+  public:
+    Mock();
+    ~Mock() {}
+
+    void increment_without_mutex();
+    void increment_with_mutex();
+    int getData() const;
+};
+
+#endif /* !MOCKCLASS_HH_ */
--- test/mutex.cpp
+++ test/mutex.cpp
@@ -1,6 +1,36 @@
+#include <iostream>
 #include <assert.h>
+#include "mockclass.hh"
 #include "Thread/Mutex.hh"
 #include "Thread/Thread.hpp"
+
+int without_mutex()
+{
+  Mock m;
+  LilWrapper::IThread *t1 = new LilWrapper::Thread(&Mock::increment_without_mutex, &m);
+  LilWrapper::IThread *t2 = new LilWrapper::Thread(&Mock::increment_without_mutex, &m);
+
+  t1->launch();
+  t2->launch();
+
+  delete t1;
+  delete t2;
+  return (m.getData());
+}
+
+int with_mutex()
+{
+  Mock m;
+  LilWrapper::IThread *t1 = new LilWrapper::Thread(&Mock::increment_with_mutex, &m);
+  LilWrapper::IThread *t2 = new LilWrapper::Thread(&Mock::increment_with_mutex, &m);
+
+  t1->launch();
+  t2->launch();
+
+  delete t1;
+  delete t2;
+  return (m.getData());
+}
 
 int main()
 {

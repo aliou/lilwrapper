77065b905f728f44979c520cd0c17f6587825eb7
Aliou Diallo <code@aliou.me>
2013-10-08 18:29:01 +0200

    Remove OS specific code.

--- include/Thread/CondVar.hh
+++ include/Thread/CondVar.hh
@@ -1,19 +1,29 @@
-#include <pthread.h>
+#include <list>
 #include "Thread/Mutex.hh"
+#include "Thread/Thread.hpp"
 
 #ifndef CONDVAR_HH_
 # define CONDVAR_HH_
 
 namespace LilWrapper
 {
+  class CondVarImpl;
+
   class CondVar
   {
     private:
-      pthread_cond_t  _condvar;
+      CondVarImpl *_impl;
 
     public:
       CondVar();
       ~CondVar();
+
+      void notify_one();
+      void notify_all();
+
+      void wait(Mutex &);
+      template <class P>
+        void wait(Mutex *, P);
   };
 }
 
--- src/Thread/CondVar.cpp
+++ src/Thread/CondVar.cpp
@@ -5,13 +5,9 @@ namespace LilWrapper
 {
   CondVar::CondVar()
   {
-    if (pthread_cond_init(&this->_condvar, NULL) != 0)
-      throw ThreadException("Thread Exception: "
-          "Error while creating the conditional variable.");
   }
 
   CondVar::~CondVar()
   {
-    pthread_cond_destroy(&this->_condvar);
   }
 }
